3e88382d49671842da6647e781c1401b3de6dba2
marvin espira <espiramarvin@gmail.com>
2022-12-11 19:55:47 +0300

    updates

---
+++ data/expenses.js
@@ -0,0 +1,62 @@
+export const EXPENSES = [
+  {
+    id: 1,
+    description: "A pair of shoes",
+    amount: 14.19,
+    date: new Date("2022-12-11"),
+  },
+  {
+    id: 2,
+    description: "A pair of trousers",
+    amount: 16.19,
+    date: new Date("2022-12-12"),
+  },
+  {
+    id: 3,
+    description: "Sony SubWoofer dz50",
+    amount: 50.19,
+    date: new Date("2023-01-02"),
+  },
+  {
+    id: 4,
+    description: "Water dispense",
+    amount: 26.19,
+    date: new Date("2022-02-14"),
+  },
+  {
+    id: 5,
+    description: "A standing desk",
+    amount: 70.19,
+    date: new Date("2022-02-22"),
+  },
+  {
+    id: 6,
+    description: "A pair of shoes",
+    amount: 14.19,
+    date: new Date("2022-12-11"),
+  },
+  {
+    id: 7,
+    description: "A pair of trousers",
+    amount: 16.19,
+    date: new Date("2022-12-12"),
+  },
+  {
+    id: 8,
+    description: "Sony SubWoofer dz50",
+    amount: 50.19,
+    date: new Date("2023-01-02"),
+  },
+  {
+    id: 9,
+    description: "Water dispense",
+    amount: 26.19,
+    date: new Date("2022-02-14"),
+  },
+  {
+    id: 10,
+    description: "A standing desk",
+    amount: 70.19,
+    date: new Date("2022-02-22"),
+  },
+]
