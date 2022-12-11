176191b8455a1fea05d9ab74aa661b10241d0a0e
marvin espira <espiramarvin@gmail.com>
2022-12-11 23:53:58 +0300

    adds reusable button

---
+++ components/ui/Button.js
@@ -0,0 +1,42 @@
+import { View, Pressable, Text, StyleSheet } from "react-native"
+import { GlobalStyles } from "../../constants/styles"
+
+export default function Button({ children, onPress, mode, style }) {
+  return (
+    <View style={style}>
+      <Pressable
+        onPress={onPress}
+        style={({ pressed }) => pressed && styles.pressed}
+      >
+        <View style={[styles.button, mode === "flat" && styles.flat]}>
+          <Text style={[styles.buttonText, mode === "flat" && styles.flatText]}>
+            {children}
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 4,
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+  },
+  flat: {
+    backgroundColor: "transparent",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  flatText: {
+    color: GlobalStyles.colors.primary100,
+  },
+  pressed: {
+    opacity: 0.75,
+    backgroundColor: GlobalStyles.colors.primary200,
+    borderRadius: 4,
+  },
+})
